b2d00367b60fa70dfb33d3558bc46cb4e3e5002b
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2025-10-22 08:28:31 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,8 @@ const client = createClient({
 export default {
 	// Remove assetPrefix and distDir for standard deployment
 	// If deploying to a subdirectory, configure in Vercel settings instead
+	assetPrefix: '/blog',
+	distDir: 'out/blog',
 	images: {
 		dangerouslyAllowSVG: true,
 		remotePatterns: [
